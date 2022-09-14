fee106c614ea6db7a57ff41380d180dbe3a14673
sinanovice <ic22b019@technikum-wien.at>
2022-09-14 19:38:58 +0200

    add hello endpoint

---
+++ HelloWorld/src/test/java/at/fhtw/bic/HelloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
